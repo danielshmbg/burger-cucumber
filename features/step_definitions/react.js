a5e345c5b1d3cb3a8ea24fe5e3a98bbc1309f7f6
Holong Daniel <dan.sihombing@gmail.com>
2019-01-14 14:40:36 +0700

    First Commit

---
+++ features/step_definitions/react.js
@@ -0,0 +1,9 @@
+const { register } = require('react-cucumber');
+// require your components here, for example:
+const {UpperCaseInput} = require('../../build/static/js/main.676ba198');
+// ...
+ 
+register([
+    UpperCaseInput
+  // ... more components here
+]);
---
+++ src/upper-case-input.js
@@ -0,0 +1,7 @@
+import React from 'react';
+ 
+export class UpperCaseInput extends React.Component {
+  render() {
+    return <input {...this.props} value={this.props.toUpperCase()} />;
+  }
+}
