d186f5564d780f3d0b7853822c6cb40969292497
Holong Daniel <dan.sihombing@gmail.com>
2019-01-14 15:12:02 +0700

    update react.js

--- features/step_definitions/react.js
+++ features/step_definitions/react.js
@@ -1,6 +1,7 @@
 const { register } = require('react-cucumber');
 // require your components here, for example:
 // assuming your build process drops your complied JS into dist/
+// const { UpperCaseInput } = require('../../dist/upper-case-input');
 const {UpperCaseInput} = require('../../build/static/js/main.676ba198');
 // ...
  
